7a460d45a9ae212de9d7fc4dfd49ea55097f7377
sajjat-ahmed <ahm.sajjat@gmail.com>
2020-12-06 22:05:02 +0600

    creative agency done

---
+++ creative-agency-client/src/Component/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjOlFxrKB_UOF_MQiXDSFDEZ_-GoPAwUk",
+  authDomain: "creative-agency-47b9d.firebaseapp.com",
+  projectId: "creative-agency-47b9d",
+  storageBucket: "creative-agency-47b9d.appspot.com",
+  messagingSenderId: "565673173719",
+  appId: "1:565673173719:web:f160d6ec719d9818d7c692",
+  measurementId: "G-JDZHPQ6F8S"
+};
+export default firebaseConfig;
